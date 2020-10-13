d52ef42a03f0021a30a512c222b43a2aa88c6598
Adam Chmara <adam.chmara1@gmail.com>
2020-10-13 14:02:07 +0200

    Add scheduling routes
    Signed-off-by: Adam Chmara <adam.chmara1@gmail.com>

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ dotenv.config();
 
 const env = process.env;
 const conf = {
-  conductorHost: env.CONDUCTOR_HOST || "conductor-server:8080/api/",
+  conductorHost: env.CONDUCTOR_HOST || "conductor-server:8080",
+  schellarHost: env.SCHELLAR_HOST || "schellar:7000"
 };
 
 module.exports = conf;
